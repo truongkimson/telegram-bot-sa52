3b39b8e491a540ca4fccfc03abf8fe284e7c95f0
Truong Kim Son <e0696689@u.nus.edu>
2021-02-12 02:35:34 +0800

    test with one-off dyno

--- src/app.py
+++ src/app.py
@@ -1,23 +1,26 @@
 import time
 import schedule
 import telegram
-import os
+# import os
 from telebot.credentials import bot_token, bot_user_name, servant_group_chat_id, test_group_chat_id
 
 
-PORT = int(os.environ.get('PORT', 5000))
+# PORT = int(os.environ.get('PORT', 5000))
 TOKEN = bot_token
 bot = telegram.Bot(token=TOKEN)
-
 
 def test_scheduled_reminder():
     msg = f"** TESTING **\nGentle reminder to submit your temperature.\nThis message is sent at {time.strftime('%H:%M:%S', time.localtime())}"
     bot.send_message(chat_id=test_group_chat_id, text=msg)
 
 
-schedule.every().day.at("02:25").do(test_scheduled_reminder)
-# schedule.every(10).seconds.do(test_scheduled_reminder)
+def main():
+    # schedule.every().day.at("02:25").do(test_scheduled_reminder)
+    schedule.every(5).seconds.do(test_scheduled_reminder)
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+if __name__ == '__main__':
+    main()
