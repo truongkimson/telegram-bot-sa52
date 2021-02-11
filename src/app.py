d36df8442285e341b2020397232e3bbece510920
Truong Kim Son <e0696689@u.nus.edu>
2021-02-12 01:24:02 +0800

    add Procfile

--- src/app.py
+++ src/app.py
@@ -13,7 +13,7 @@ def test_scheduled_reminder():
     bot.send_message(chat_id=test_group_chat_id, text=msg)
 
 
-schedule.every().day.at("01:22").do(test_scheduled_reminder)
+schedule.every().day.at("01:26").do(test_scheduled_reminder)
 
 
 while True:
