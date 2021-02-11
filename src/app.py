c2a6ecd35daa939a4881b5c623b7326f557ed540
Truong Kim Son <e0696689@u.nus.edu>
2021-02-12 02:01:09 +0800

    add PORT:5000

--- src/app.py
+++ src/app.py
@@ -15,7 +15,7 @@ def test_scheduled_reminder():
     bot.send_message(chat_id=test_group_chat_id, text=msg)
 
 
-schedule.every().day.at("02:00").do(test_scheduled_reminder)
+schedule.every().day.at("02:03").do(test_scheduled_reminder)
 # schedule.every(5).seconds.do(test_scheduled_reminder)
 
 while True:
