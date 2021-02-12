139f36e6fc819e271b19447eb9c406eccf0487f5
Truong Kim Son <e0696689@u.nus.edu>
2021-02-12 14:21:08 +0800

    test APScheduler cron

--- src/clock.py
+++ src/clock.py
@@ -12,8 +12,12 @@ def test_scheduled_reminder():
     msg = f"** TESTING **\nGood Morning! This message is scheduled every morning.\nThis message was sent at {time.strftime('%H:%M:%S', time.localtime())}\nHosted on Heroku."
     bot.send_message(chat_id=test_group_chat_id, text=msg)
 
-@sched.scheduled_job('interval', seconds=5)
-def test_every_5_seconds():
+# @sched.scheduled_job('interval', seconds=5)
+# def test_interval():
+#     test_scheduled_reminder()
+
+@sched.scheduled_job('cron', days_of_week='mon-fri', hour=14, minute=22)
+def test_cron():
     test_scheduled_reminder()
 
 sched.start()
