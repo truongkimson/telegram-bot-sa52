41673d1a44bb5f8463bd997e94615c6f1ffa12c9
Truong Kim Son <e0696689@u.nus.edu>
2021-02-12 14:53:52 +0800

    deploy to heroku

--- src/clock.py
+++ src/clock.py
@@ -1,6 +1,9 @@
 import time
 import telegram
-from telebot.credentials import bot_token, bot_user_name, servant_group_chat_id, test_group_chat_id
+from telebot.credentials import (bot_token,
+                                servant_group_chat_id,
+                                test_group_chat_id,
+                                sa52_group_chat_id, )
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 
@@ -8,16 +11,23 @@ TOKEN = bot_token
 bot = telegram.Bot(token=TOKEN)
 sched = BlockingScheduler()
 
-def test_scheduled_reminder():
-    msg = f"** TESTING **\nGood Morning! This message is scheduled every morning.\nThis message was sent at {time.strftime('%H:%M:%S', time.localtime())}\nHosted on Heroku."
-    bot.send_message(chat_id=test_group_chat_id, text=msg)
+# def test_scheduled_reminder():
+#     msg = f"Hi All,\nRemember to submit your temperature before class today \U0001F60A.\n\n{time.strftime('%H:%M:%S', time.localtime())}"
+#     bot.send_message(chat_id=test_group_chat_id, text=msg)
+
+
+def scheduled_temperature_reminder():
+    msg = f"Hi All,\nRemember to submit your temperature before class today \U0001F60A.\n\n{time.strftime('%H:%M:%S', time.localtime())}"
+    bot.send_message(chat_id=sa52_group_chat_id, text=msg)
+
 
 # @sched.scheduled_job('interval', seconds=5)
 # def test_interval():
 #     test_scheduled_reminder()
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=14, minute=27)
-def test_cron():
-    test_scheduled_reminder()
+
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=8, minute=0)
+def cron():
+    scheduled_temperature_reminder()
 
 sched.start()
--- src/telebot/credentials.py
+++ src/telebot/credentials.py
@@ -2,4 +2,5 @@ bot_token = '1680505017:AAGydqwuKAKU0A-iP2kP7s7CGN4TVySOVVc'
 bot_user_name = 'sa52_bot'
 test_group_chat_id = -580518273
 servant_group_chat_id = -1001450618706
+sa52_group_chat_id = -1001274542597
 URL = ''
