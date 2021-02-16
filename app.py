6017c9e23f82d06b19dc4289b886ad405321a4c2
Truong Kim Son <e0696689@u.nus.edu>
2021-02-17 02:33:19 +0800

    debug

--- app.py
+++ app.py
@@ -21,26 +21,28 @@ def set_webhook():
 
 @app.route(f'/{TOKEN}', methods=['POST'])
 def respond():
-    update = telegram.Update.de_json(request.get_json(force=True), bot)
+    print(request.get_json())
+    # update = telegram.Update.de_json(request.get_json(force=True), bot)
 
-    chat_id = update.message.chat.id
-    msg_id = update.message.message_id
-    update_id = update.update_id
 
-    text = update.message.text.encode('utf-8').decode()
-    print(update_id, text)
+#     chat_id = update.message.chat.id
+#     msg_id = update.message.message_id
+#     update_id = update.update_id
 
-    if text == '/start':
-        welcome_msg = '''
-Hi there!
-I'm Ale's assistant.
-        '''
-        bot.send_message(chat_id=chat_id, text=welcome_msg, reply_to_message_id=msg_id)
+#     text = update.message.text.encode('utf-8').decode()
+#     print(update_id, text)
 
-    if text == '/hello':
-        user_first_name = update.message.from_user.first_name
-        hello_msg = f'Hello {user_first_name}!'
-        bot.send_message(chat_id=chat_id, text=hello_msg, reply_to_message_id=msg_id)
+#     if text == '/start':
+#         welcome_msg = '''
+# Hi there!
+# I'm Ale's assistant.
+#         '''
+#         bot.send_message(chat_id=chat_id, text=welcome_msg, reply_to_message_id=msg_id)
+
+#     if text == '/hello':
+#         user_first_name = update.message.from_user.first_name
+#         hello_msg = f'Hello {user_first_name}!'
+#         bot.send_message(chat_id=chat_id, text=hello_msg, reply_to_message_id=msg_id)
 
     return 'ok'
 
