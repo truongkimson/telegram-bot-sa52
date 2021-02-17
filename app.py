d126247a39c9ab2870a6c7bb834f44a043f07f65
Truong Kim Son <e0696689@u.nus.edu>
2021-02-17 20:43:08 +0800

    add /command@bot_usernamehandle

--- app.py
+++ app.py
@@ -42,12 +42,12 @@ I'm Ale's assistant.
         '''
         bot.send_message(chat_id=chat_id, text=welcome_msg, reply_to_message_id=msg_id)
 
-    elif text == '/hello':
+    elif text == '/hello' or text == f'/hello@{bot_user_name}':
         user_first_name = update.message.from_user.first_name
         hello_msg = f'Hello {user_first_name}!'
         bot.send_message(chat_id=chat_id, text=hello_msg, reply_to_message_id=msg_id)
 
-    elif text == '/meme':
+    elif text == '/meme' or text == f'/meme@{bot_user_name}':
         url = meme.get_random_meme()
         bot.send_photo(chat_id=chat_id, photo=url)
         
