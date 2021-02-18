316fac8615f98beb805410d77ffd71ed7824189f
Truong Kim Son <e0696689@u.nus.edu>
2021-02-18 10:32:12 +0800

    correct marketCap display

--- telebot/stock.py
+++ telebot/stock.py
@@ -13,18 +13,17 @@ def get_quote(symbol):
     # intraday = requests.get(f'https://cloud.iexapis.com/stable/stock/{symbol}/intraday-prices?chartLast=1&token={iex_token}')
     # parsed_intraday = json.loads(intraday.text)
 
-    quote_string = f'''
-    <b>${parsed_quote["symbol"]} {parsed_quote["companyName"]}</b>
-    <b>${parsed_quote["latestPrice"]:,.2f}</b>
-    Previous close: ${parsed_quote["previousClose"]:,.2f}
-    Change: ${parsed_quote["change"]:,.2f}      % Change: {parsed_quote["changePercent"]:.2%}
-    Market cap: {int(parsed_quote["marketCap"]/10e6):,}M    P/E: {parsed_quote["peRatio"]}
-    Data by <a href="https://iexcloud.io">IEX Cloud</a>
-    '''
     # with open('quote.json', 'w') as f:
     # f.write(json.dumps(parsed_quote, indent=4, sort_keys=True))
 
     # with open('intraday.json', 'w') as f:
     # f.write(json.dumps(parsed_intraday, indent=4, sort_keys=True))
-    
-    return quote_string
+        quote_string = f'''
+        <b>${parsed_quote["symbol"]} {parsed_quote["companyName"]}</b>
+        <b>${parsed_quote["latestPrice"]:,.2f}</b>
+        Previous close: ${parsed_quote["previousClose"]:,.2f}
+        Change: ${parsed_quote["change"]:,.2f}      % Change: {parsed_quote["changePercent"]:.2%}
+        Market cap: {int(parsed_quote["marketCap"]/10e5):,}M    P/E: {parsed_quote["peRatio"]}
+        Data by <a href="https://iexcloud.io">IEX Cloud</a>
+        '''
+        return quote_string
