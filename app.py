531cc1b9504ae05b86c649c0db5ca9baa71d23f9
Truong Kim Son <e0696689@u.nus.edu>
2021-02-19 22:27:51 +0800

    add /punish_hard handle

--- app.py
+++ app.py
@@ -62,6 +62,9 @@ Commands available
         user_first_name = update.message.from_user.first_name
         punish_msg = 'Yamete kudasai~'
         bot.send_message(chat_id=chat_id, text=punish_msg, reply_to_message_id=msg_id)
+
+    elif text == '/punish_hard' or text == f'/punish_hard@{bot_user_name}':
+        user_first_name = update.message.from_user.first_name
         try:
             bot.send_voice(chat_id=chat_id, voice=yamete_file_id, reply_to_message_id=msg_id)
         except Exception as e:
