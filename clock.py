05830af591454ca8553500e5ccc1c83889386f08
Truong Kim Son <e0696689@u.nus.edu>
2021-02-19 21:11:05 +0800

    add scheduling to send message to test group every day

--- clock.py
+++ clock.py
@@ -11,23 +11,19 @@ TOKEN = bot_token
 bot = telegram.Bot(token=TOKEN)
 sched = BlockingScheduler()
 
-# def test_scheduled_reminder():
-#     msg = f"Hi All,\nRemember to submit your temperature before class today \U0001F60A.\n\n{time.strftime('%H:%M:%S', time.localtime())}"
-#     bot.send_message(chat_id=test_group_chat_id, text=msg)
-
-
-def scheduled_temperature_reminder():
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=8, minute=0)
+def temperature_reminder():
     msg = f"Hi All,\nRemember to submit your temperature before class today \U0001F60A.\n\n{time.strftime('%H:%M:%S', time.localtime())}"
     bot.send_message(chat_id=sa52_group_chat_id, text=msg)
 
+@sched.scheduled_job('cron', hour=8, minute=0)
+def temperature_reminder_everyday():
+    msg = f"Hi All,\nRemember to submit your temperature before class today \U0001F60A.\n\n{time.strftime('%H:%M:%S', time.localtime())}"
+    bot.send_message(chat_id=test_group_chat_id, text=msg)
 
-# @sched.scheduled_job('interval', seconds=5)
-# def test_interval():
-#     test_scheduled_reminder()
-
-
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=8, minute=0)
-def cron():
-    scheduled_temperature_reminder()
+@sched.scheduled_job('cron', hour=21, minute=15)
+def test():
+    msg = f"Hi All,\nRemember to submit your temperature before class today \U0001F60A.\n\n{time.strftime('%H:%M:%S', time.localtime())}"
+    bot.send_message(chat_id=test_group_chat_id, text=msg)
 
 sched.start()
