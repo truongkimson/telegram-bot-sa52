faedc386c822cdde1a0c36b54e96150e9aedaf26
Truong Kim Son <e0696689@u.nus.edu>
2021-03-01 20:02:14 +0800

    testing MS power automate triggers

--- app.py
+++ app.py
@@ -117,7 +117,7 @@ def set_webhook():
 @app.route('/luminus_announcement', methods=['POST'])
 def luminus_announcement():
     if request.method == 'POST':
-        print(request.get_json())
+        print(request.form)
     return 'ok'
 
 
