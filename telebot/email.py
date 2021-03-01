104df298135d35c6b5362e32a644bb5ca4a6ccfe
Truong Kim Son <e0696689@u.nus.edu>
2021-03-01 20:51:39 +0800

    testing MS power automate triggers

--- telebot/email.py
+++ telebot/email.py
@@ -1,9 +1,9 @@
 from .credentials import test_group_chat_id, servant_group_chat_id
 
 def get_luminus_announcement(email):
-    sender = email.sender
-    subject = email.subject
-    body = email.body
+    sender = email['sender']
+    subject = email['subject']
+    body = email['body']
 
     announce_message = f"""
 Update from {sender}!
