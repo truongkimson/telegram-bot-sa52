02487ca0a92bfb18f11ecc0a4942233e9fe98bec
Truong Kim Son <e0696689@u.nus.edu>
2021-03-04 10:16:39 +0800

    modularise scheduled announcement

--- clock.py
+++ clock.py
@@ -5,6 +5,7 @@ from telebot.credentials import (bot_token,
                                 test_group_chat_id,
                                 sa52_group_chat_id, )
 from apscheduler.schedulers.blocking import BlockingScheduler
+from telebot.announcement import temp_reminder
 
 
 TOKEN = bot_token
@@ -14,16 +15,12 @@ sched = BlockingScheduler()
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=8, minute=0)
 def temperature_reminder():
     msg = f"Hi All,\nRemember to submit your temperature before class today \U0001F60A.\n\n{time.strftime('%H:%M:%S', time.localtime())}"
-    bot.send_message(chat_id=sa52_group_chat_id, text=msg)
+    result = bot.send_message(chat_id=sa52_group_chat_id, text=msg)
+    print(result)
 
-@sched.scheduled_job('cron', hour=8, minute=0)
+@sched.scheduled_job('cron', hour=10, minute=18)
 def temperature_reminder_everyday():
     msg = f"Hi All,\nRemember to submit your temperature before class today \U0001F60A.\n\n{time.strftime('%H:%M:%S', time.localtime())}"
-    bot.send_message(chat_id=test_group_chat_id, text=msg)
-
-@sched.scheduled_job('cron', hour=21, minute=15)
-def test():
-    msg = f"Hi All,\nRemember to submit your temperature before class today \U0001F60A.\n\n{time.strftime('%H:%M:%S', time.localtime())}"
-    bot.send_message(chat_id=test_group_chat_id, text=msg)
+    temp_reminder(test_group_chat_id, msg)
 
 sched.start()
---
+++ telebot/announcement.py
@@ -0,0 +1,10 @@
+import telegram
+from telebot.credentials import bot_token
+
+
+TOKEN = bot_token
+bot = telegram.Bot(token=TOKEN)
+
+def temp_reminder(chat_id, msg):
+    result = bot.send_message(chat_id=chat_id, text=msg)
+    print(result)
