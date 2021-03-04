fd44df7a472de419d3061bf7ec2e9c843bfb7f92
Truong Kim Son <e0696689@u.nus.edu>
2021-03-04 11:21:10 +0800

    refactor sched adding job

--- clock.py
+++ clock.py
@@ -18,18 +18,22 @@ job_schedule = [
         'trigger': 'cron',
         'dow': '*',
         'hour': 11,
-        'minute': 17,
+        'minute': 22,
         'func': send_announcement,
         'msg': f"Hi All,\nRemember to submit your temperature before class today \U0001F60A.\n\n{time.strftime('%H:%M:%S', time.localtime())}",
         'chat_id': test_group_chat_id
+    },
+    {
+        'id': 'reminder0',
+        'trigger': 'cron',
+        'dow': 'mon-fri',
+        'hour': 8,
+        'minute': 0,
+        'func': send_announcement,
+        'msg': f"Hi All,\nRemember to submit your temperature before class today \U0001F60A.\n\n{time.strftime('%H:%M:%S', time.localtime())}",
+        'chat_id': sa52_group_chat_id
     }
 ]
-
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=8, minute=0)
-def temperature_reminder():
-    msg = f"Hi All,\nRemember to submit your temperature before class today \U0001F60A.\n\n{time.strftime('%H:%M:%S', time.localtime())}"
-    result = bot.send_message(chat_id=sa52_group_chat_id, text=msg)
-    print(result)
 
 for j in job_schedule:
     sched.add_job(j['func'], j['trigger'], day_of_week=j['dow'], hour=j['hour'], minute=j['minute'], id='test0', args=(j['chat_id'], j['msg']))
