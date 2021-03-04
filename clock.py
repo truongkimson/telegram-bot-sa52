28421aa658018ea4ba97b4fb7695d2e94d4435ef
Truong Kim Son <e0696689@u.nus.edu>
2021-03-04 11:11:48 +0800

    refactor sched adding job

--- clock.py
+++ clock.py
@@ -12,15 +12,25 @@ TOKEN = bot_token
 bot = telegram.Bot(token=TOKEN)
 sched = BlockingScheduler()
 
+job_schedule = [
+    {
+        'id': 'test0',
+        'trigger': 'cron',
+        'dow': '*',
+        'hour': 11,
+        'minute': 14,
+        'func': send_announcement,
+        'msg': f"Hi All,\nRemember to submit your temperature before class today \U0001F60A.\n\n{time.strftime('%H:%M:%S', time.localtime())}",
+        'chat_id': test_group_chat_id
+    }
+]
+
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=8, minute=0)
 def temperature_reminder():
     msg = f"Hi All,\nRemember to submit your temperature before class today \U0001F60A.\n\n{time.strftime('%H:%M:%S', time.localtime())}"
     result = bot.send_message(chat_id=sa52_group_chat_id, text=msg)
     print(result)
 
-def temperature_reminder_everyday():
-    msg = f"Hi All,\nRemember to submit your temperature before class today \U0001F60A.\n\n{time.strftime('%H:%M:%S', time.localtime())}"
-    send_announcement(test_group_chat_id, msg)
-
-sched.add_job(temperature_reminder_everyday, 'cron', hour=10, minute=50, id='test-1')
+for j in job_schedule:
+    sched.add_job(j['func'], j['trigger'], day_of_work=j['dow'], hour=j['hour'], minute=j['minute'], id='test0', args=(j['chat_id'], j['msg']))
 sched.start()
