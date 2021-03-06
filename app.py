5cc00d44619ac60486556ec970aaa597e8a17f19
Truong Kim Son <e0696689@u.nus.edu>
2021-03-06 23:09:46 +0800

    add gmail client to handle push notifications from Google Pub-Sub

--- app.py
+++ app.py
@@ -225,7 +225,7 @@ def oauth2callback():
 @app.route('/gmail/revoke')
 def revoke():
     if not os.path.exists('gmail/gmail_token.pickle'):
-        return ('You need to <a href="gmail/authorize">authorize</a> before testing the code to revoke credentials.')
+        return ('You need to <a href="/gmail/authorize">authorize</a> before testing the code to revoke credentials.')
 
     with open('gmail/gmail_token.pickle', 'rb') as token:
         creds = pickle.load(token)
@@ -280,22 +280,22 @@ def luminus_announcement():
 
 def print_index_table():
   return ('<table>' +
-          '<tr><td><a href="/test">Test an API request</a></td>' +
+          '<tr><td><a href="test">Test an API request</a></td>' +
           '<td>Submit an API request and see a formatted JSON response. ' +
           '    Go through the authorization flow if there are no stored ' +
           '    credentials for the user.</td></tr>' +
-          '<tr><td><a href="/authorize">Test the auth flow directly</a></td>' +
+          '<tr><td><a href="authorize">Test the auth flow directly</a></td>' +
           '<td>Go directly to the authorization flow. If there are stored ' +
           '    credentials, you still might not be prompted to reauthorize ' +
           '    the application.</td></tr>' +
-          '<tr><td><a href="/revoke">Revoke current credentials</a></td>' +
+          '<tr><td><a href="revoke">Revoke current credentials</a></td>' +
           '<td>Revoke the access token associated with the current user ' +
           '    session. After revoking credentials, if you go to the test ' +
           '    page, you should see an <code>invalid_grant</code> error.' +
           '</td></tr>' +
-          '<tr><td><a href="/clear">Clear Flask session credentials</a></td>' +
+          '<tr><td><a href="clear">Clear Flask session credentials</a></td>' +
           '<td>Clear the access token currently stored in the user session. ' +
-          '    After clearing the token, if you <a href="/test">test the ' +
+          '    After clearing the token, if you <a href="test">test the ' +
           '    API request</a> again, you should go back to the auth flow.' +
           '</td></tr></table>')
 
