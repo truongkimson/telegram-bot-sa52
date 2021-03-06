7e2922b2af9b740d8e0b5a4a66dc282d232134fc
Truong Kim Son <e0696689@u.nus.edu>
2021-03-07 03:56:36 +0800

    format message output

--- app.py
+++ app.py
@@ -282,12 +282,12 @@ def luminus_announcement():
                                 for part in att.walk():
                                     if 'From' in part:
                                         msg += f'Update from: {part.get("From")}\n'
-                                        msg += f'Subject {part.get("Subject")}\n'
                                         received_date = datetime.strptime(part.get('Date'), '%a, %d %b %Y %H:%M:%S %z')
                                         msg += received_date.strftime('%a, %d %b, %y %H:%M\n')
+                                        msg += f'Subject: {part.get("Subject")}\n'
                                     if (part.get_content_type() == 'text/plain'):
                                         msg += trim_message(part.get_content())
-                                        msg = msg[:200] + '--truncated'
+                                        msg = msg[:200] + ' --truncated'
                                         print(msg)
                                         bot.send_message(chat_id=test_group_chat_id, text=msg)
         history_id = history_list['historyId']
