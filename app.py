e559a2b2a866d64002204208c6d0d6f6b29146ae
truongkimson <60567409+truongkimson@users.noreply.github.com>
2021-03-11 18:20:59 +0800

    delete old codes from response()

--- app.py
+++ app.py
@@ -58,79 +58,7 @@ def index():
 def respond():
     update = telegram.Update.de_json(request.get_json(), bot)
     print(request.get_json())
-
     handler(bot, update)
-#     try:
-#         if update.message:
-#             update_message = update.message
-#         elif update.edited_message:
-#             update_message = update.edited_message
-#         else:
-#             return 'ok'
-#         chat_id = update_message.chat.id
-#         msg_id = update_message.message_id
-#         text = update_message.text.encode('utf-8').decode()
-#     except AttributeError as e:
-#         print(e)
-#         print(update_message)
-#         return 'ok'
-
-#     if text == '/start' or text == f'/start@{bot_user_name}':
-#         welcome_msg = '''
-# Hi there!
-# I'm Ale's assistant.
-# '''
-#         bot.send_message(chat_id=chat_id, text=welcome_msg,
-#                          reply_to_message_id=msg_id)
-
-#     elif text == '/help' or text == f'/help@{bot_user_name}':
-#         help_msg = '''
-# Commands available
-# /help - Show help
-# /hello - Say hello to you
-# /meme - Send a random meme scraped from reddit
-# /stock - Check stock price. Usage: /stock [symbol]
-# '''
-#         bot.send_message(chat_id=chat_id, text=help_msg,
-#                          reply_to_message_id=msg_id)
-
-#     elif text == '/hello' or text == f'/hello@{bot_user_name}':
-#         user_first_name = update.message.from_user.first_name
-#         hello_msg = f'Hello {user_first_name}!'
-#         bot.send_message(chat_id=chat_id, text=hello_msg,
-#                          reply_to_message_id=msg_id)
-
-#     elif text == '/punish' or text == f'/punish@{bot_user_name}':
-#         user_first_name = update.message.from_user.first_name
-#         punish_msg = f'Yamete kudasai~ Sama {user_first_name}'
-#         bot.send_message(chat_id=chat_id, text=punish_msg,
-#                          reply_to_message_id=msg_id)
-
-#     elif text == '/punish_hard' or text == f'/punish_hard@{bot_user_name}':
-#         user_first_name = update.message.from_user.first_name
-#         try:
-#             bot.send_voice(chat_id=chat_id, voice=yamete_file_id,
-#                            reply_to_message_id=msg_id)
-#         except Exception as e:
-#             print(e)
-
-#     elif text == '/meme' or text == f'/meme@{bot_user_name}':
-#         url = meme.get_random_meme()
-#         bot.send_photo(chat_id=chat_id, photo=url)
-
-#     elif text.startswith('/stock') or text.startswith(f'/stock@{bot_user_name}'):
-#         try:
-#             symbol = text.strip().split()[1]
-#             quote_msg = stock.get_quote(symbol)
-#         except IndexError:
-#             quote_msg = "Command usage: /stock [symbol]"
-#         bot.send_message(chat_id=chat_id, text=quote_msg, reply_to_message_id=msg_id,
-#                          parse_mode='HTML', disable_web_page_preview=True)
-
-#     elif update.message.reply_to_message:
-#         reply_msg = 'Sorry I don\'t understand'
-#         bot.send_message(chat_id=chat_id, text=reply_msg,
-#                          reply_to_message_id=msg_id)
     return 'ok'
 
 
